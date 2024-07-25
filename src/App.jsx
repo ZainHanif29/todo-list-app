0c2a5e59077982d68f4c861ec3a86a9ca4449650
ZainHanif29 <zainhanif2902@gmail.com>
2024-07-25 17:23:04 +0500

    Todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo,setTodo] = useState([]);
+  const [data,setData] = useState('');
+
+  const add = (e)=>{
+    e.preventDefault();
+    setTodo([...todo,[data,new Date().toLocaleTimeString()]])
+    setData('');
+  }
 
   return (
     <>
-<h1>TODO LIST</h1>
+      <main>
+        <section>
+          <section>
+            <h1>TODO LIST APP</h1>
+          </section>
+          <section>
+            <form onSubmit={add}>
+            <input type="text" placeholder="add todo" value={data} onChange={(e)=>{setData(e.target.value)}} />
+            <button type="submit">Add</button>
+            </form>
+          </section>
+          <section>
+            <table>
+             <thead>
+             <tr>
+                <th>#</th>
+                <th>TODO</th>
+                <th>Time</th>
+              </tr>
+             </thead>
+              <tbody>
+              {todo.map((data,index)=>(
+                <tr key={index}>
+                <td>{index+1}</td>
+                <td>{data[0]}</td>
+                <td>{data[1]}</td>
+              </tr>
+              ))}
+              </tbody>
+            </table>
+          </section>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
